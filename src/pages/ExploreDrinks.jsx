6e6f6661eb811606346dc56f17a0828a8b675f34
killu4kun <mateeus.soarees@outlook.com>
2021-11-09 19:02:04 -0300

    Tela de explorar/comida e explorar/drinks

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function ExploreDrinks() {
+  const history = useHistory();
   return (
     <div>
       <Header title="Explorar Bebidas" showSearch={ false } />
-      <h1> ExploreDrinks </h1>
+      <button
+        type="button"
+        onClick={ () => history.push('/explorar/bebidas/ingredientes') }
+        data-testid="explore-by-ingredient"
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        testid="explore-surprise"
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
