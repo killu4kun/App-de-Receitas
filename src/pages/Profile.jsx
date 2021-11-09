e44b13b0b9e7b73b3be8e56b59fc7bec05e68a42
killu4kun <mateeus.soarees@outlook.com>
2021-11-09 16:57:00 -0300

    Profile Perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
+  const history = useHistory();
   return (
     <div>
       <Header title="Perfil" showSearch={ false } />
-      <h1> Profile </h1>
+      <h1>
+        { email }
+      </h1>
+      <button type="button" onClick={ () => history.push('/receitas-feitas') }>
+        Receitas Feitas
+      </button>
+      <button type="button" onClick={ () => history.push('/receitas-favoritas') }>
+        Receitas Favoritas
+      </button>
+      <button
+        type="button"
+        onClick={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+      >
+        Sair
+      </button>
       <Footer />
     </div>
   );
