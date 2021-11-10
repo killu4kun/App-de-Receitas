b043d212189c5aef7e948a425007703f4369b4ea
killu4kun <mateeus.soarees@outlook.com>
2021-11-10 16:05:11 -0300

    imp 67 ao 74

--- src/pages/ExploreDrinks.jsx
+++ src/pages/ExploreDrinks.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import ContainerExplorar from '../components/ContainerExplorar';
 
 function ExploreDrinks() {
-  const history = useHistory();
   return (
     <div>
       <Header title="Explorar Bebidas" showSearch={ false } />
-      <button
-        type="button"
-        onClick={ () => history.push('/explorar/bebidas/ingredientes') }
-        data-testid="explore-by-ingredient"
-      >
-        Por Ingredientes
-      </button>
-      <button
-        type="button"
-        testid="explore-surprise"
-      >
-        Me Surpreenda!
-      </button>
+      <ContainerExplorar type="bebidas" />
       <Footer />
     </div>
   );
--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,33 +1,13 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import ContainerExplorar from '../components/ContainerExplorar';
 
 function ExploreFood() {
-  const history = useHistory();
   return (
     <div>
       <Header title="Explorar Comida" showSearch={ false } />
-      <button
-        type="button"
-        onClick={ () => history.push('/explorar/comidas/ingredientes') }
-        data-testid="explore-by-ingredient"
-      >
-        Por Ingredientes
-      </button>
-      <button
-        type="button"
-        onClick={ () => history.push('/explorar/comidas/area') }
-        data-testid="explore-by-area"
-      >
-        Por Local de Origem
-      </button>
-      <button
-        type="button"
-        testid="explore-surprise"
-      >
-        Me Surpreenda!
-      </button>
+      <ContainerExplorar type="comidas" />
       <Footer />
     </div>
   );
