bc13c0c43fe3294e1db711658098ff5d89fb1d21
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-11-11 17:01:02 -0300

    fix header problems

--- src/pages/ExploreFoodArea.jsx
+++ src/pages/ExploreFoodArea.jsx
@@ -5,7 +5,7 @@ import Footer from '../components/Footer';
 function ExploreFoodArea() {
   return (
     <div>
-      <Header title="Explorar Origem" showSearch={ false } />
+      <Header title="Explorar Origem" showSearch />
       <h1> ExploreFoodArea </h1>
       <Footer />
     </div>
