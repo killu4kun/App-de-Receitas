02344cf737a9d71d167358fa90a42f5ad352a995
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-11-17 21:44:31 -0300

    solve requirement 50

--- src/components/ListIngredients.jsx
+++ src/components/ListIngredients.jsx
@@ -4,41 +4,40 @@ import '../css/foodInProgress.css';
 import { useLocation } from 'react-router';
 
 const ListIngredients = ({ ingredients }) => {
-  const [compareChecked, setCompareChecked] = useState({});
-  const [isChecked, setIsChecked] = useState({});
   const { pathname } = useLocation();
   const regex = /\d+/g;
   const [locationID] = pathname.match(regex);
-
-  const setLocalStorage = () => {
-    const meals = {
-      [locationID]: [compareChecked],
-    };
-    localStorage.setItem('meals', JSON.stringify(meals));
+  const handleChecked = () => {
+    // retorna true or undefined
+    if (localStorage.getItem('meals')) {
+      const recipe = localStorage.getItem('meals');
+      const recipeJSon = JSON.parse(recipe)[locationID];
+      const recipeChecked = recipeJSon[0];
+      return recipeChecked;
+    }
   };
+  const [compareChecked, setCompareChecked] = useState(localStorage.getItem('meals') ? handleChecked() : {});
+
+  useEffect(() => {
+    const setLocalStorage = () => {
+      const meals = {
+        [locationID]: [compareChecked],
+      };
+      localStorage.setItem('meals', JSON.stringify(meals));
+    };
+    setLocalStorage();
+  }, [compareChecked, locationID]);
 
   const handleCheckboxControl = (index, checked) => {
     setCompareChecked((prevState) => ({
       ...prevState,
       [index]: checked,
     }));
-    setLocalStorage();
   };
 
-  useEffect(() => {
-    const handleChecked = () => {
-      // retorna true or undefined
-      if (localStorage.getItem('meals')) {
-        const recipe = localStorage.getItem('meals');
-        const recipeJSon = JSON.parse(recipe)[locationID];
-        const recipeChecked = recipeJSon[0];
-        setIsChecked(recipeChecked);
-      }
-    };
+  handleChecked();
 
-    handleChecked();
-  }, [compareChecked]);
-
+  console.log(compareChecked);
   return (
     <ol>
       {ingredients.map((ingredient, index) => (
@@ -47,13 +46,13 @@ const ListIngredients = ({ ingredients }) => {
           key={ index }
         >
           <label
-            className={ isChecked[index] ? 'decoration' : null }
+            className={ compareChecked[index] ? 'decoration' : null }
             htmlFor={ `${index}-ingredients-checkbox` }
           >
             <input
               id={ `${index}-ingredients-checkbox` }
               type="checkbox"
-              checked={ isChecked[index] }
+              checked={ compareChecked[index] }
               onChange={ ({ target: { checked } }) => {
                 handleCheckboxControl(index, checked);
               } }
