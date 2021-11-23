2d9b9a806614a0eada6e2a1594045300befba45f
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-11-23 10:11:08 -0300

    revert css on searchBar, Login and Header

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from 'react';
 import { useLocation } from 'react-router';
-import { Button, Nav, InputGroup, Form, FormControl } from 'react-bootstrap';
+import Input from './Input';
+import Button from './Button';
 import RecipeContext from '../context/RecipeContext';
 
 function SearchBar() {
@@ -8,91 +9,56 @@ function SearchBar() {
     handleInputChange, handleRadioChange } = useContext(RecipeContext);
   const { pathname } = useLocation();
   const locationRoute = pathname.slice(1);
-
   useEffect(() => {
     setLocationName(locationRoute);
   }, []);
-
-  return (
-    <Nav fill="true" justify="true">
-      <Form>
-        <InputGroup>
-          <FormControl
-            placeholder="O que você deseja comer?"
-            aria-label="O que você deseja comer?"
-            data-testid="search-input"
-            onChange={ ({ target: { value } }) => handleInputChange(value) }
-          />
-        </InputGroup>
-        <Form.Check
-          inline
-          label="1"
+  
+​  return (
+    <>
+      <Input
+        type="text"
+        dataTestId="search-input"
+        name="search-input"
+        onChange={ ({ target: { value } }) => handleInputChange(value) }
+      />
+      <label
+        htmlFor="chosen-filter"
+        onChange={ ({ target: { value } }) => handleRadioChange(value) }
+      >
+        <input
           name="chosen-filter"
           type="radio"
           id="ingredient"
           value="ingredient"
           data-testid="ingredient-search-radio"
-          onChange={ ({ target: { value } }) => handleRadioChange(value) }
         />
-        <Form.Check
-          inline
-          label="2"
+        Ingrediente
+        <input
           name="chosen-filter"
           type="radio"
           id="name"
           value="name"
           data-testid="name-search-radio"
-          onChange={ ({ target: { value } }) => handleRadioChange(value) }
         />
-        <Form.Check
-          inline
-          label="3"
+        Nome
+        <input
           name="chosen-filter"
           type="radio"
           id="first-letter"
           value="first-letter"
           data-testid="first-letter-search-radio"
-          onChange={ ({ target: { value } }) => handleRadioChange(value) }
         />
-
-        {/* <label
-          htmlFor="chosen-filter"
-          onChange={ ({ target: { value } }) => handleRadioChange(value) }
-        >
-          <Input
-            name="chosen-filter"
-            type="radio"
-            id="ingredient"
-            value="ingredient"
-            data-testid="ingredient-search-radio"
-          />
-          Ingrediente
-          <Input
-            name="chosen-filter"
-            type="radio"
-            id="name"
-            value="name"
-            data-testid="name-search-radio"
-          />
-          Nome */}
-        {/* <input
-              name="chosen-filter"
-              type="radio"
-              id="first-letter"
-              value="first-letter"
-              data-testid="first-letter-search-radio"
-            />
-            Primeira letra
-          </label> */}
-        <Button
-          text="Buscar"
-          variant="outline-sucess"
-          onClick={ handleClick }
-          dataTestId="exec-search-btn"
-        />
-      </Form>
-    </Nav>
+        Primeira letra
+      </label>
+      <Button
+        text="Buscar"
+        variant="outline-sucess"
+        onClick={ handleClick }
+        dataTestId="exec-search-btn"
+      />
+    </>
+​
   );
 }
-
+​
 export default SearchBar;
